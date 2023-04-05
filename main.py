b896234aba252e8f706e17f847087946627f053e
Kofi <pastorkofi101@gmail.com>
2023-04-04 21:21:49 -0400

    Set up font.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+# Import the modules
 import pygame
 from my_colors import *
 
+
+# Set up window
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 screen.fill(GRAY)
+pygame.display.set_caption("Math Practice for Kids")
+
+# Fonts
+sysfont = pygame.font.get_default_font()
+print('system font :', sysfont)
+font = pygame.font.SysFont(None, 48)
+img1 = font.render('Math Practice', True, RED)
 
 running = True
 
@@ -11,5 +21,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(img1, (125, 0))
     pygame.display.update()
 pygame.quit()
