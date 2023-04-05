733c6b651d86dc6e8ed43f4ad2960e52c007d6ad
Kofi <pastorkofi101@gmail.com>
2023-04-04 20:55:30 -0400

    Setup.

--- main.py
+++ main.py
@@ -1,55 +1,15 @@
-#!"C:\Python310\python.exe"
-from tkinter import *
+import pygame
+from my_colors import *
 
-# Setup game
-root = Tk()
-root.title("Math Quiz")
-root.geometry("500x500")
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+screen.fill(GRAY)
 
-givenAnswer = IntVar()
+running = True
 
-# Title 
-titleLabel = Label(
-    text="Math Quiz",
-    font=("Arial Black", 40),
-    bg="yellow",
-    relief=SUNKEN,
-    borderwidth=10)
-titleLabel.grid(row=0, column=0)
-
-
-# Question Label
-questionLabel = Label(
-    text="question",
-    font=("Arial", 40),
-    bg="#83c167",
-    relief=SUNKEN,
-    borderwidth=10)
-questionLabel.grid(row=1, column=0)
-
-# Answer Entry
-answerEntry = Entry(
-    textvariable=givenAnswer,
-    font=("Arial Bold", 30),
-    bg="#58c4dd",
-    relief=SUNKEN,
-    borderwidth=10,
-)
-answerEntry.grid(row=2, column=0)
-# Submit Button
-submitButton = Button(
-    text="Submit",
-    font=("Carlito Bold", 30),
-    bg="white",
-    relief=RAISED,
-    borderwidth=10
-)
-submitButton.grid(row=3, column=0)
-# Score Label
-scoreLabel = Label(
-    text= "0 / 10",
-    font=("Arial Black", 40),
-)
-scoreLabel.grid(row=4, column=0)
-# Game loop
-root.mainloop()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+pygame.quit()
